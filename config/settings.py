bfd3146056c881238167fcf7c9ae5fbbfc802aaf
pplmtv <pplmtv@gmail.com>
2021-11-14 08:55:08 +0900

    config/settings.py updated

--- config/settings.py
+++ config/settings.py
@@ -137,9 +137,11 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
 }
 
-# CORES
-CORES_ALLOW_ALL_ORIGINS = False
-CORES_ALLOWED_ORIGINS = [
+# CORS
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
     'http://localhost:8080',
     'http://127.0.0.1:8080',
+    'http://127.0.0.1:8000',
 ]
