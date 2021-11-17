a0687cfc5a9d10449db446f69556678de4e55f5f
pplmtv <pplmtv@gmail.com>
2021-11-18 07:01:22 +0900

    config/urls.py updated

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 from django.views.generic import TemplateView, RedirectView
 
+from django.conf import settings
+import debug_toolbar
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html')),
@@ -10,3 +13,6 @@ urlpatterns = [
     path('api/v1/', include('apiv1.urls')),
     re_path('', RedirectView.as_view(url='/')),
 ]
+
+if settings.DEBUG:
+    urlpatterns = [path('__debug__/', include(debug_toolbar.urls)), ] + urlpatterns
