60dc4287525cbfb2e836a30dde6008b2ac333267
pplmtv <pplmtv@gmail.com>
2022-01-30 18:44:43 +0900

    frontend/server.js

--- frontend/server.js
+++ frontend/server.js
@@ -1,14 +1,23 @@
 const http = require('http');
+const fs = require('fs').promises
 
 const hostname = '127.0.0.1';
 const port = 8080;
 
-const server = http.createServer((reeq, res) => {
+const server = http.createServer((req, res) => {
+	// const url = "public" + req.url;
 	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/plain');
-	res.end('Hello World\n');
+	// res.setHeader('Content-Type', 'text/plain');
+	// res.end('Hello World\n');
+	// fs.readFile(__dirname + "/index.html")
+	fs.readFile("../templates" + "/index.html")
+		.then(contents => {
+			res.setHeader("Content-Type", "text/html");
+			res.writeHead(200);
+			res.end(contents);
+		})
 });
 
 server.listen(port, hostname, () => {
-	console.log('Server running at http://${hostname}:${port}/');
+	console.log('Server running');
 });
